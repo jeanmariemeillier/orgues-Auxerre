d1f6c7aee95d5337cf5d32711c70ade366d215ac
renemaure <pascalrollin290@gmail.com>
2023-05-21 19:58:45 +0200

    modif install blog

--- systeme/js/orgues_auxerre.js
+++ systeme/js/orgues_auxerre.js
@@ -1,5 +1,5 @@
 var bebuttop =230;//position de début du top de ID
-var tempo = 100; //temps de la boucle settimeout
+var tempo = 150; //temps de la boucle settimeout
 var fintop = -350; // position mini du top de ID
 var pasdefil =1;  //pas de decopmte dela valeur de top du ID
 var tp = bebuttop; 
