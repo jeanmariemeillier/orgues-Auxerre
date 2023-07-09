e62f864dd9185414f8ac19a4c1d66b3f587a7ac0
jean marie meillier <jeanmariemeillier89@gmail.com>
2023-07-09 12:00:25 +0200

    travail sur acceuil ce dimanche

--- pages/accueil.php
+++ pages/accueil.php
@@ -1,27 +1,22 @@
 <div id="accueil">
    <section>
-   <article id="menu_accueil">
-   <ul>
-      <li class="trt_accueil">BATIMENTS DISPARUS</li>
-      <li><a href="index.php?pg=10">Les Cordeliers</a></li>
-      <li><a href="index.php?pg=11">Les Frères Précheurs</a></li>
-      <li><a href="index.php?pg=12">Notre-Dame la-d'Hors</a></li>
-      
-      <li>EGLISES ACTUELLES</li>
-      <li>
-         <a href="index.php?pg=13">
-            Saint-Eusèbe l'orgue de choeur
-         </a>
-      </li>
-      <li>
-         <a href="index.php?pg=14">
-            Saint-Eusèbe le grand-orgue
-         </a>
-      </li>
-      <li></li>
-      <li></li>
-   </ul>
-  </article>
+      <article id="menu_accueil">
+         <ul>
+            <?php
+            	for ($menu=10; $menu <=$liens["nbr_menu_acc"]; $menu++)
+               { 
+                 if ($liens["indic".$menu]["valid"]==1)
+                  { 
+                     echo "<li><a href=\"".$liens["indic".$menu]["lien_pg"]."\">".$liens["indic".$menu]["trt_menu"]."</a></li>";
+                  }
+                  elseif ($liens["indic".$menu]["valid"]==0)
+                  {
+                     echo"<li class=\"".$liens["indic".$menu]["lien_pg"]."\">".$liens["indic".$menu]["trt_menu"]."</li>";
+                  }
+               }
+            ?>
+         </ul>
+      </article>
    </section>
    <section>
       <figure id="puzzle">
@@ -30,13 +25,6 @@
       </figure>   
    </section>
    <section id="blog">
-   <?php include($liens["dirlien"]."/textes_blog/actu_01.php");?>
-      <!-- <p>
-         Ceci n'est pas un site web, du moins, pas encore ; <font color="#FF0000">c'est un feuilleton</font>, dont chacun des �pisodes est complet (sauf d�couvertes nouvelles).
-      </p>
-      <p>
-		 Certaines des donn�es qui le composent ou le composeront sont sur deux blogs int�ressant les Orgues de l'Yonne.
-      </p> -->
-      </article>
+      <?php include($liens["dirlien"]."/textes_blog/actu_01.php");?>
    </section>
 </div>
