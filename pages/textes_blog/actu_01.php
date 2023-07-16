b13ca5495edb1cf79621b5064e2090f98a7e2aa0
renemaure <pascalrollin290@gmail.com>
2023-07-16 08:58:54 +0200

    modif pascal blog page accueil

--- pages/textes_blog/actu_01.php
+++ pages/textes_blog/actu_01.php
@@ -1,9 +1,11 @@
-<article id="Actu">
+<article id="blog_actu">
+    <div id="Actu">
         <p>ACTUALITES</p>
+    </div>
         <div id="ActuContenant"> 
           <div id="ActuTexte"> 
             <?php include "pages/elements/info_accueil.php"; ?>
           </div>
         </div>
-      </article>
-<article id="txt_accueil">
+</article>
+<!-- <article id="txt_accueil"> -->
