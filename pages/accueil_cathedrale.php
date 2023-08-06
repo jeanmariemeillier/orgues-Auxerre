3300b90880eb099ebe5201b62bef158044c12835
jean marie meillier <jeanmariemeillier89@gmail.com>
2023-08-06 12:09:20 +0200

    sauvegarde de ce dimanche

---
+++ pages/accueil_cathedrale.php
@@ -0,0 +1,25 @@
+<section id="titre_cath">
+    <h1>Les orgues de la Cathédrale d'Auxerre</h1>
+</section>
+
+<div id="accueil_cath">
+   <section id="menu_cath">
+        <!-- <article id="menu_cath">
+
+        </article> -->
+    <ul>
+        <li class="trt_accueil indic10">Le grand orgue</li>
+        <li class="indic11 "><a href="index.php?pg=11">Les Cordeliers</a></li>
+        <li class="indic12 "><a href="index.php?pg=12">Les Frères Précheurs</a></li>
+        <li class="indic13 "><a href="index.php?pg=13">Notre-Dame la-d'Hors</a></li>
+        <li class="trt_accueil indic14">L'orgue de choeur</li>
+        <li class="indic15 "><a href="index.php?pg=15">Saint-Eusèbe l'orgue de choeur</a></li>
+        <li class="indic16 "><a href="index.php?pg=16">Saint-Eusèbe le grand-orgue</a></li>
+        <li class="indic17 "><a href="index.php?pg=17">Saint-Pierre en Vallée</a></li>
+    </ul>
+    </section>
+    <section id="milieu_cath">
+
+    </section>
+</div>
+
