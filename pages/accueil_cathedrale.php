6f0d6de8537ff22471b65a55fd57048540de4a34
renemaure <pascalrollin290@gmail.com>
2023-08-27 09:22:23 +0200

    modif pascal menu cathédrale

--- pages/accueil_cathedrale.php
+++ pages/accueil_cathedrale.php
@@ -4,24 +4,23 @@
 
 <div id="accueil_cath">
    <section id="menu_cath">
-        <ul>
-            <li class="indic30 "><a href="#">Préface</a></li>
+        <ul id="nav_cath">
+            <li class="indic30"><a href="#">Préface</a></li>
             <li class="trt_accueil indic31">Histoire des orgues</li>
         
             <li class="indic32">
                 <a href="#">Des origines à 1789</a>
-                <ul class="sous_mn1">
+                <ul class="sous_menu_cath">
                     <li><a class="" href="#">Chapitre 1</a></li>
                     <li><a href="#">Chapitre 2</a></li>
                     <li><a href="#">Chapitre 3</a></li>
                 </ul>
-                
             </li>
-            <li class="indic13 "><a href="index.php?pg=13">Notre-Dame la-d'Hors</a></li>
-            <li class="trt_accueil indic14">L'orgue de choeur</li>
-            <li class="indic15 "><a href="index.php?pg=15">Saint-Eusèbe l'orgue de choeur</a></li>
-            <li class="indic16 "><a href="index.php?pg=16">Saint-Eusèbe le grand-orgue</a></li>
-            <li class="indic17 "><a href="index.php?pg=17">Saint-Pierre en Vallée</a></li>
+            <li class="indic33 "><a href="index.php?pg=13">Notre-Dame la-d'Hors</a></li>
+            <li class="trt_accueil indic34">L'orgue de choeur</li>
+            <li class="indic35 "><a href="index.php?pg=15">Saint-Eusèbe l'orgue de choeur</a></li>
+            <li class="indic36 "><a href="index.php?pg=16">Saint-Eusèbe le grand-orgue</a></li>
+            <li class="indic37 "><a href="index.php?pg=17">Saint-Pierre en Vallée</a></li>
         </ul>
     </section>
     <section id="milieu_cath">
