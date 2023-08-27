d0f144c7c05e3c2fd836bb115591d89dac2f158a
jean marie meillier <jeanmariemeillier89@gmail.com>
2023-08-27 12:39:14 +0200

    sauvegarde de ce dimanche

--- pages/accueil_cathedrale.php
+++ pages/accueil_cathedrale.php
@@ -5,9 +5,15 @@
 <div id="accueil_cath">
    <section id="menu_cath">
         <ul id="nav_cath">
-            <li class="indic30"><a href="#">Préface</a></li>
+        <li class="indic30">
+                <a href="#">Préface</a>
+                <ul class="sous_menu_cath">
+                    <li><a class="" href="#">La cathédrale </a></li>
+                    <li><a href="#"></a>Musique religieuse </li>
+                    <li><a href="#"></a>Note d’art et d’histoire </li>
+                </ul>
+            </li>
             <li class="trt_accueil indic31">Histoire des orgues</li>
-        
             <li class="indic32">
                 <a href="#">Des origines à 1789</a>
                 <ul class="sous_menu_cath">
@@ -16,11 +22,43 @@
                     <li><a href="#">Chapitre 3</a></li>
                 </ul>
             </li>
-            <li class="indic33 "><a href="index.php?pg=13">Notre-Dame la-d'Hors</a></li>
-            <li class="trt_accueil indic34">L'orgue de choeur</li>
-            <li class="indic35 "><a href="index.php?pg=15">Saint-Eusèbe l'orgue de choeur</a></li>
-            <li class="indic36 "><a href="index.php?pg=16">Saint-Eusèbe le grand-orgue</a></li>
-            <li class="indic37 "><a href="index.php?pg=17">Saint-Pierre en Vallée</a></li>
+            <li class="indic33">
+                <a href="#">De 1789 à 1900</a>
+                <ul class="sous_menu_cath">
+                    <li><a class="" href="#">Chapitre 1</a></li>
+                    <li><a href="#">Chapitre 2</a></li>
+                    <li><a href="#">Chapitre 3</a></li>
+                </ul>
+            </li>
+            <li class="indic34">
+                <a href="#">De 1900 aux soumissions </a>
+                <ul class="sous_menu_cath">
+                    <li><a class="" href="#">Chapitre 1</a></li>
+                    <li><a href="#">Chapitre 2</a></li>
+                </ul>
+            </li>
+            <li class="indic35">
+                <a href="#">Inauguration de l’orgue </a>
+                <ul class="sous_menu_cath">
+                    <li><a class="" href="#">Chapitre 1</a></li>
+                    <li><a href="#">Chapitre 2</a></li>
+                    <li><a href="#">Chapitre 3</a></li>
+                    <li><a href="#">Chapitre 4</a></li>
+                </ul>
+            </li>
+            <li class="trt_accueil indic36">Albums photos </li>
+            <li class="indic37"><a href="#">Photos avant 1983 </a></li>
+            <li class="indic38">
+                <a href="#">Photos à partir de 1985</a>
+                <ul class="sous_menu_cath">
+                    <li><a class="" href="#">Album 1</a></li>
+                    <li><a href="#">Album 2</a></li>
+                    <li><a href="#">Album 3</a></li>
+                    <li><a href="#">Album 4</a></li>
+                    <li><a href="#">Album 5</a></li>
+                    <li><a href="#">Album 6</a></li>
+                </ul>
+            </li>
         </ul>
     </section>
     <section id="milieu_cath">
