7d3fd663af704f005e1b57a0b5e76f57f9c8006c
renemaure <pascalrollin290@gmail.com>
2023-10-29 22:10:40 +0100

    déplacement fichiers blog en dossier modules/blog

--- systeme/blog/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*header("Location: ../index.php"); */
-/* Redirection du navigateur en local*/
-
-header("Location: http://reperechoppe89.com/");
-/* Redirection du navigateur sur le serveur */
-exit;
-?>
