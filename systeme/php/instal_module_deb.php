5c39c005bd1bf62f9233611332d3a6d77f42219a
renemaure <pascalrollin290@gmail.com>
2023-11-01 08:56:28 +0100

    ajout de déclaration du module tooltip dans lemoteur du collectif

--- systeme/php/instal_module_deb.php
+++ systeme/php/instal_module_deb.php
@@ -1,11 +1,19 @@
 <?php
     /* 
         fichier d'installation des différents modules dansle moteur du collectif
-        création au 30/10/2023 version 1.1.0
-        fonctionne uniquement sur la version 5 du moteur
+        création au 30/10/2023 
+        installation du module Blog
+        actuellement à la version 1.1.1 au 31/10/2023
+        ajout du module Tooltip
+        
+        ! fonctionne uniquement sur la version 5 du moteur !
     */
     if ($liens["mod_blog"]) {
-        $json = file_get_contents("modules/blog/module_blog.json");
+        $json = file_get_contents( $demar["dos_modul"]."blog/".$liens["js_blog"].".json");
         $mod_blog = json_decode($json, true);
+    }
+    if ($liens["mod_tooltip"]) {
+        $json = file_get_contents($demar["dos_modul"]."tooltip/".$liens["js_tooltip"].".json");
+        $mod_tooltip = json_decode($json, true);
     }
 ?>
